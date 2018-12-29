b5695491d72c1a631e49e41ff2fb12bdbd9b66a3
serendipitylee <33026863+serendipitylee@users.noreply.github.com>
2018-12-29 14:37:53 +0800

    aaaaaaaaaaaaa

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-SITE_NAME = '论坛'
-AUTHOR = 'RussellLuo'
+SITE_NAME = '嘤嘤'
 
-GLOBAL_PARAMS = {'site_name': SITE_NAME, 'title': SITE_NAME, 'author': AUTHOR}
+GLOBAL_PARAMS = {'site_name': SITE_NAME, 'title': SITE_NAME }
 
 ##### 公共配置 #####
 COOKIE_EXPIRES = 3600 # 单位s
@@ -13,7 +12,11 @@ POSTS_PER_PAGE = 10 # 每页显示10篇文章
 
 # 本地环境下的MySQL配置
 MYSQL_USERNAME = 'root'
+<<<<<<< Updated upstream
 MYSQL_PASSWORD = 'zx111111'
+=======
+MYSQL_PASSWORD = 'Jaylee19980417'
+>>>>>>> Stashed changes
 
 ##### email服务器配置 #####
 import web
